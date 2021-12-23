e4c0681965ff7f2e46aecf6bb33b7d31187784ab
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-23 16:57:02 +0100

    Changes to level 3

--- Assets/Scripts/TerminalHandler.cs
+++ Assets/Scripts/TerminalHandler.cs
@@ -2,22 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.IO.Ports;
 
 public class TerminalHandler : MonoBehaviour
 {
    
-    public GameObject activatedTerminalScreen, hackedTerminalScreen;
+    private GameObject initScreen, activatedTerminalScreen, hackedTerminalScreen;
 
-    public GameObject activatedTerminalScreenPreselectedButton, hackedTerminalScreenPreselectedButton;
+    private GameObject initScreenPreselectedButton, activatedTerminalScreenPreselectedButton, hackedTerminalScreenPreselectedButton;
+
+    // private Text...
+
+    // The name of the serial port to use to communicate with
+    // the Arduino.
+    public string serialPort;
+
+    public int baudrate;
+
+    private SerialPort _serialPort;
+
+    // True during the initialization phase.
+    private bool initializing = true;
 
     public void Start()
     {
+        // Some initialization.
+        initScreen = transform.Find("Init").gameObject;
+        activatedTerminalScreen = transform.Find("ActivatedTerminal").gameObject;
+        hackedTerminalScreen = transform.Find("hackedTerminal").gameObject;
+
+        initScreenPreselectedButton = initScreen.transform.Find("CancelButton").gameObject;
+        activatedTerminalScreenPreselectedButton = activatedTerminalScreen.transform.Find("QuitButton").gameObject;
+        hackedTerminalScreenPreselectedButton = hackedTerminalScreen.transform.Find("IDInput").gameObject;
+
         activatedTerminalScreen.SetActive(false);
         hackedTerminalScreen.SetActive(false);
+        initScreen.SetActive(true);
+
+        _serialPort = new SerialPort(serialPort, baudrate);
+        _serialPort.Open();
+
+        // Tells the Arduino to initiate the init phase.
+        _serialPort.WriteLine("INIT");
     }
-    public void activateTerminal()
+
+    public void Update()
+    {
+        if (!initializing || _serialPort.BytesToRead <= 0) return;
+
+        // The Arduino has successfully finished its initialization phase.
+        if (_serialPort.ReadLine() == "OK")
+        {
+            initializing = false;
+            initScreen.SetActive(false);
+        }
+    }
+    public void activateTerminal(Terminal terminal)
     {
         Time.timeScale = 0;
+        initScreen.SetActive(false);
         hackedTerminalScreen.SetActive(false);
         activatedTerminalScreen.SetActive(true);
         EventSystem.current.SetSelectedGameObject(null);
@@ -26,6 +69,7 @@ public class TerminalHandler : MonoBehaviour
 
     public void hackTerminal()
     {
+        initScreen.SetActive(false);
         activatedTerminalScreen.SetActive(false);
         hackedTerminalScreen.SetActive(true);
         EventSystem.current.SetSelectedGameObject(null);
@@ -34,6 +78,7 @@ public class TerminalHandler : MonoBehaviour
 
     public void closeTerminal()
     {
+        initScreen.SetActive(false);
         activatedTerminalScreen.SetActive(false);
         hackedTerminalScreen.SetActive(false);
         Time.timeScale = 1;
