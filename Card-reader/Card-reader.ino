aaa56e4420d58ad68244036c9ca4a4d40ee59f43
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-24 14:23:10 +0100

    Cleaning of the Arduino script

--- Card-reader/Card-reader.ino
+++ Card-reader/Card-reader.ino
@@ -9,15 +9,6 @@
 
 MFRC522 mfrc522(RC522_SDA, RC522_RST);
 
-byte bufferIn[16] = "Hello";
-byte bufferOut[18];
-
-//int block = 2;
-
-MFRC522::MIFARE_Key key;
-
-MFRC522::StatusCode status;
-
 // LCD setup.
 
 #define LCD_RS D2
@@ -61,7 +52,6 @@ void lcdPrint(String const message);
 void lcdClear();
 
 void setup() {
-
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
   digitalWrite(LED_GREEN, LOW);
@@ -71,97 +61,23 @@ void setup() {
   Serial.begin(115200);
   while (!Serial);
 
-  ledBlink(true, true, 10, 100);
-  lcdPrint("My name is Giovanni Giorgio!");
-  delay(5000);
-  lcdPrint("All your base are belong to us!");
-  delay(5000);
-  lcdPrint("azeazazeazeazeazeazezedgd");
-  delay(5000);
-  lcdPrint("Hello!");
-  delay(1000);
-  lcdClear();
-
   // Initialisation du Module RFID.
   SPI.begin();
   mfrc522.PCD_Init();
 
   mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
-
-  // No need.
-  // Affichage des données de la bibliothèque.
-  mfrc522.PCD_DumpVersionToSerial();
-
-  Serial.println(F("Scan PICC to see UID, type, and data blocks..."));
-
 }
 
 void loop() {
-  
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
-
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  if (!mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
   // Une carte a été présentée !
-
-  // On affiche le numéro de série de la carte.
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
+  if (!mfrc522.PICC_ReadCardSerial()) {
     return;
   }
-
   // Affichage de l'état de la mémoire de la carte.
   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-
-  for (int block = 0; block < 63; block++) {
-    if((block + 1) % 4 == 0) {continue;}
-    String message = String("--- Attempting to authenticate to data block ") + String(block) + String(" with key A...");
-    Serial.println(message);
-    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
-    if (status != MFRC522::STATUS_OK)
-    {
-      Serial.print("Authentication failed: ");
-      Serial.println(mfrc522.GetStatusCodeName(status));
-      //return;
-    }
-    else
-    {
-      Serial.println("SUCCESS!!!");
-    }
-    message = String("--- Attempting to authenticate to data block ") + String(block) + String(" with key B...");
-    Serial.println(message);
-    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_B, block, &key, &(mfrc522.uid));
-    if (status != MFRC522::STATUS_OK)
-    {
-      Serial.print("Authentication failed: ");
-      Serial.println(mfrc522.GetStatusCodeName(status));
-      //return;
-    }
-    else
-    {
-      Serial.println("SUCCESS!!!");
-    }
-  }
-
-  /* Write data to the block */
-  /*
-  status = mfrc522.MIFARE_Write(block, bufferIn, 16);
-  if (status != MFRC522::STATUS_OK)
-  {
-    Serial.print("Writing to Block failed: ");
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-  else
-  {
-    Serial.println("Data was written into Block successfully");
-  }
-
-  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-  */
-
 }
 
 void serialEvent() {
